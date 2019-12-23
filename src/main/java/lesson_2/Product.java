bad086c85a41f1f4701befeb71f63d3b68dbd417
shaitanshamma <kurbatov_andr@hotmail.com>
2019-12-23 22:08:55 +0300

    Add Product class

---
+++ src/main/java/lesson_2/Product.java
@@ -0,0 +1,31 @@
+package lesson_2;
+
+public class Product {
+    private int id;
+    private String title;
+    private int cost;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
